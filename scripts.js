e078ad36ff42a5f46bd23506987478f43e7498d1
Mohamed Dounnani <mohamed.dounnani@weroad.it>
2020-04-06 11:32:57 +0200

    cv

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function handleButtonClick() {
+  const door = document.getElementById('door')
+  const button = document.getElementById('button')
+  const typewriter = document.getElementById('typewriter')
+  const myContent = document.getElementById('m-content')
+  button.classList.add('invisible')
+  typewriter.classList.add('invisible')
+  myContent.classList.remove('invisible')
+  if(door.classList.contains('door')) {
+    door.classList.add('door-open')
+  }
+}
